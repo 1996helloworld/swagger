13dff25fbca8e10d43f4ee6edf67a9e2c75eaf19
于杰 吴 <157348414@qq.com>
2018-08-07 16:38:02 +0800

    数据库链接

---
+++ AccountApi/Controllers/JdDataController.cs
@@ -0,0 +1,36 @@
+﻿using Business;
+using DataAccess;
+using Entity;
+using Repository;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace AccountApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JdDataController : ApiController
+    {
+        /// <summary>
+        /// 公用方法
+        /// </summary>
+        public readonly RepositoryFactory<BaseInfo> repositoryfactory = new RepositoryFactory<BaseInfo>();
+        /// <summary>
+        /// 获取所有商品
+        /// </summary>
+        /// <returns></returns>
+        [System.Web.Mvc.HttpGet]
+        public List<BaseInfo> GetAll()
+        {
+            return repositoryfactory.Repository().FindList();
+        }
+    }
+}
---
+++ Entity/BaseInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    /// <summary>
+    /// 京东商品
+    /// </summary>
+    public class BaseInfo
+    {
+        //[Id], [Title], [Image], [Store], [Comment], [Url], [CreateDate]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Store { get; set; }
+        public string Comment { get; set; }
+        public string Url { get; set; }
+        public string CreateDate { get; set; }
+    }
+}
